2e10fd53445959d7b7539a1e0821197be45c22a7
Alex Chopard <alex.chopard1998@gmail.com>
2019-03-03 21:07:52 +0100

    First commit

---
+++ app/src/main/java/com/albasheep/motivation/MotivationActivity.kt
@@ -0,0 +1,12 @@
+package com.albasheep.motivation
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MotivationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
